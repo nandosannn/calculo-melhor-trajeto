f7d0dd594e229e3969f92ea4979daf9ce6480a76
nandosannn <silvafernandosan@gmail.com>
2024-08-09 09:16:58 -0300

    filtro-tipo-transporte

--- src/model/Grafo.cpp
+++ src/model/Grafo.cpp
@@ -8,16 +8,16 @@ using namespace std;
 // Implementação da classe Grafo
 Grafo::Grafo(int V)
 {
-	this->V = V;  // Atribui o número de vértices (cidades cadastradas)
+	this->V = V;							 // Atribui o número de vértices (cidades cadastradas)
 	adj = new list<tuple<int, int, int>>[V]; // Corrigido para usar tuplas
 }
 
 Grafo::~Grafo()
 {
-	delete[] adj;  // Libera a memória alocada
+	delete[] adj; // Libera a memória alocada
 }
 
-void Grafo::adicionarAresta(vector<Trajeto*> trajetos)
+void Grafo::adicionarAresta(vector<Trajeto *> trajetos)
 {
 	// Adiciona vértice v2 à lista de vértices adjacentes de v1 com a distância e tipo de transporte
 	for (size_t i = 0; i < trajetos.size(); i++)
@@ -34,51 +34,58 @@ void Grafo::adicionarAresta(vector<Trajeto*> trajetos)
 
 void Grafo::dfs(int v, int w, int tipoTransporte)
 {
-	stack<tuple<int, int, int>> pilha; // Corrigido para usar tuplas
+	vector<tuple<int, int, int>> pilha; // Usar vector em vez de stack
+	bool visitando[V];
 
 	// Vetor de visitados
-	vector<bool> visitados(V, false);  // Usar vector<bool> para simplificar a inicialização
-
-	// Adiciona o vértice inicial na pilha
-	pilha.push(make_tuple(v, 0, tipoTransporte));
-
-	while (!pilha.empty())
+	for ( int i = 0; i < V; i++)
 	{
-		tuple<int, int, int> p = pilha.top(); // Pega o topo da pilha
-		pilha.pop();						  // Remove o topo da pilha
-
-		int verticeAtual = get<0>(p);
-		int distanciaAcumulada = get<1>(p);
-		int tipoAtual = get<2>(p);
-
-		// Marca o vértice como visitado se ainda não foi
-		if (!visitados[verticeAtual])
-		{
-			cout << "Visitando vértice " << verticeAtual << " com distância acumulada " << distanciaAcumulada << " e tipo de transporte " << tipoAtual << "...\n";
-			visitados[verticeAtual] = true;
-		}
-
-		// Percorre os vizinhos
-		for (auto& it : adj[verticeAtual])
-		{
-			int vizinho = get<0>(it);
-			int distancia = get<1>(it);
-			int tipo = get<2>(it);
-
-			// Se o vizinho é o destino e o tipo de transporte é o desejado, imprime a distância e termina
-			if (vizinho == w && tipo == tipoTransporte)
-			{
-				cout << "Destino " << w << " encontrado com distância total " << distanciaAcumulada + distancia << " e tipo de transporte " << tipo << "!\n";
-				return;
-			}
-
-			// Se não visitado, adiciona na pilha com a distância acumulada
-			if (!visitados[vizinho])
-			{
-				pilha.push(make_tuple(vizinho, distanciaAcumulada + distancia, tipo));
-			}
-		}
+		visitando[i] = false;
 	}
+	
+	// Adiciona o vértice inicial na pilha
+	tuple<int, int, int> tupla;
+	int distanciaAcumulada = 0;
+	while (true)
+	{
+		if (!visitando[v])
+		{
+			cout << "Visitando vertice " << v << ", distancia acumulada: " << distanciaAcumulada << ", tipo do trajeto: " << tipoTransporte << endl;
+			visitando[v] = true; // marca como visitado
+			pilha.push_back(make_tuple(v, distanciaAcumulada, tipoTransporte)); // insere "v" na pilha
+		}
 
-	cout << "Caminho para o destino " << w << " não encontrado com o tipo de transporte " << tipoTransporte << ".\n";
+		bool achou = false;
+		list<tuple<int, int, int>>::iterator it;
+		
+		// Percorre os vizinhos
+		for (it = adj[v].begin(); it != adj[v].end(); it++)
+		{
+			if (!visitando[get<0>(*it)] && get<2>(*it) == tipoTransporte)
+			{
+				achou = true;
+				break;
+			}
+			
+		}
+
+		if (achou)
+		{
+			v = get<0>(*it);
+			distanciaAcumulada += get<1>(*it);
+			tipoTransporte = get<2>(*it);
+		}
+		else{
+			pilha.pop_back();
+			
+			if (pilha.empty())
+			{
+				break;
+			}
+			tupla = pilha.back();
+			v = get<0>(tupla);
+			tipoTransporte = get<2>(tupla);
+		}
+		
+	}
 }
